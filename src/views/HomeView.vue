9f591b8cc6988dc329734998368f23feb313a511
AbsintheSung <h90243768@gmail.com>
2024-07-13 13:24:15 +0800

    feat:建立home頁的基本布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <script setup></script>
 
 <template>
-  <main></main>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="btn btn-outline-success" type="submit">登出</button>
+    </div>
+  </nav>
+  <div class="container-fluid vh-100 row">
+    <div class="row">
+      <aside class="col-1 border-end pt-5">我是測攔</aside>
+      <main class="col pt-5">我是內容</main>
+    </div>
+  </div>
 </template>
