47a7d5398a253fd07edd206ee2a3634c991ef2f2
AbsintheSung <h90243768@gmail.com>
2024-07-13 23:22:24 +0800

    feat:撰寫Modal顯示以及測試

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,17 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import ModalView from '../components/ModalView.vue'
+const modal = ref(null)
+</script>
 
 <template>
+  <ModalView ref="modal"></ModalView>
+  <ModalView ref="modals"></ModalView>
   <div class="vh-100 d-flex flex-column">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="btn btn-outline-success" type="submit">登出</button>
+        <button class="btn btn-outline-success" @click="modal.handleOpen">登出</button>
       </div>
     </nav>
     <div class="container-fluid flex-grow-1">
