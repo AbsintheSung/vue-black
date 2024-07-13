1950add8d3bab9077edb5a9e31505970711d4491
AbsintheSung <h90243768@gmail.com>
2024-07-13 23:01:52 +0800

    feat:modal設定

---
+++ src/plugins/bootstrap.modal.js
@@ -0,0 +1,22 @@
+import { ref, onMounted } from 'vue'
+import Modal from 'bootstrap/js/dist/modal';
+export function useModal() {
+    const modalOption = ref(null)
+    const modal = ref(null)
+
+    onMounted(() => {
+        modal.value = new Modal(modalOption.value)
+    })
+
+    const myModalShow = () => {
+        modal.value.show()
+    }
+
+    const myModalClose = () => {
+        modal.value.hide()
+    }
+
+    return {
+        myModalShow, myModalClose, modal, modalOption
+    }
+}
