1ae8bc02443756101b4a22aef59de221f64f6b50
AbsintheSung <h90243768@gmail.com>
2024-07-13 23:24:28 +0800

    update:刪除測試的一些內容

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,11 @@
-<script setup>
-import { ref } from 'vue'
-import ModalView from '../components/ModalView.vue'
-const modal = ref(null)
-</script>
+<script setup></script>
 
 <template>
-  <ModalView ref="modal"></ModalView>
-  <ModalView ref="modals"></ModalView>
   <div class="vh-100 d-flex flex-column">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="btn btn-outline-success" @click="modal.handleOpen">登出</button>
+        <button class="btn btn-outline-success">登出</button>
       </div>
     </nav>
     <div class="container-fluid flex-grow-1">
