74b1eb91df7cae47a88ac636b32f632f0a286387
AbsintheSung <h90243768@gmail.com>
2024-07-14 22:27:36 +0800

    feat:新增ProductList表格內容

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,5 +1,38 @@
 <script setup></script>
 <template>
-  <div>我是產品列表</div>
+  <div class="text-end">
+    <button class="btn btn-primary" type="button">新增產品</button>
+  </div>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th>分類</th>
+        <th>產品名稱</th>
+        <th>原價</th>
+        <th>售價</th>
+        <th>是否啟用</th>
+        <th>功能</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>A分類</td>
+        <td>A名稱</td>
+        <td class="text-right">A價格</td>
+        <td class="text-right">B價格</td>
+        <td>
+          <span class="text-success">啟用</span>
+          <span class="text-danger">未啟用</span>
+        </td>
+        <td>
+          <div class="d-flex align-items-center">
+            <button class="btn btn-outline-primary">編輯</button>
+            <button class="btn btn-outline-danger ms-2">刪除</button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 <style scoped></style>
