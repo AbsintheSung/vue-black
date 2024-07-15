61abf847b7f1c35b47ab6c5ed6e6aa85df98c354
AbsintheSung <h90243768@gmail.com>
2024-07-16 00:04:42 +0800

    feat:安裝bootstrap-icon並且重新配置日曆樣式

--- src/components/CouponModal.vue
+++ src/components/CouponModal.vue
@@ -28,10 +28,33 @@ const date = ref(new Date())
             <div class="row gx-2 mb-3">
               <div class="mb-3 col-md-8">
                 <label for="time" class="form-label">到期日</label>
-                <DatePicker v-model="date" is-dark="true">
-                  <template #default="{ inputValue, inputEvents }">
-                    <input type="text" class="form-control" id="time" :value="inputValue" v-on="inputEvents" />
-                    <!-- <BaseInput :value="inputValue" v-on="inputEvents" /> -->
+                <DatePicker v-model="date" :popover="false" is-dark="true">
+                  <template #default="{ togglePopover, inputValue, inputEvents }">
+                    <div class="input-group">
+                      <span class="input-group-text" id="basic-addon1" @click="() => togglePopover()">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-calendar"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"
+                          ></path>
+                        </svg>
+                      </span>
+                      <input
+                        :value="inputValue"
+                        v-on="inputEvents"
+                        type="text"
+                        class="form-control"
+                        placeholder="Input group example"
+                        aria-label="Input group example"
+                        aria-describedby="basic-addon1"
+                      />
+                    </div>
                   </template>
                 </DatePicker>
               </div>
