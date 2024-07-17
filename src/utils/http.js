ff37d4ee52b67e30b0d076c5840ad54b94ce531a
AbsintheSung <h90243768@gmail.com>
2024-07-18 02:17:11 +0800

    feat:create-logout-logic

--- src/utils/http.js
+++ src/utils/http.js
@@ -20,7 +20,7 @@ axios.interceptors.response.use(function (response) {
 }, function (error) {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
-    if (error.response.status === 401) {
+    if (error.response.status === 401 || error.response.status === 403) {
         router.push('/login')
     }
     // console.log(error.response.status)
